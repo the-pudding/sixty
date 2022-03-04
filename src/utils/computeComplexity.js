9117605ca9df4410d81db517e6e8fbe17fc2706d
arjunkakkar8 <arjunkakkar8@gmail.com>
2022-03-04 10:48:40 -0800

    Added complexity calculation script

---
+++ src/utils/computeComplexity.js
@@ -0,0 +1,46 @@
+import { getScore } from './supabase';
+
+const m = {
+    "toss": 32.5,
+    "roll": 36.62,
+    "spot": 41.41,
+}
+
+const s = {
+    "toss": 1.56,
+    "roll": 1.04,
+    "spot": 0.93,
+}
+
+const a = {
+    "toss": 2,
+    "roll": 6,
+    "spot": 9,
+}
+
+const normalizeString = (str) => {
+    const elements = str.split('');
+    const unique_elements = elements
+        .reduce((acc, curr) => acc.includes(curr) ? acc : acc + curr, "")
+        .split('');
+    const indices = elements.map((element) => unique_elements.indexOf(element));
+    return indices.join('');
+}
+
+const computeScore = async (string, task) => {
+    const normalized_string = normalizeString(string);
+    const raw_score = await getScore(normalized_string, a[task]);
+    const normalized_score = (raw_score - m[task]) / s[task];
+
+    return normalized_score;
+}
+
+export default async function conscomputeCombinedScore(string_toss, string_roll, string_spot) {
+    const [toss_score, roll_score, spot_score] = await Promise.all([
+        computeScore(string_toss, "toss"),
+        computeScore(string_roll, "roll"),
+        computeScore(string_spot, "spot"),
+    ]);
+
+    return (toss_score + roll_score + spot_score) / 3;
+}
--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -9,3 +9,15 @@ export const insert = async ({ table, data }) => {
 	const response = await supabase.from(table).insert([data]);
 	if (response.error) console.log(response.error);
 };
+
+export const getScore = async (id, a) => {
+	const response = await supabase
+		.from('acss')
+		.select(`K_${a}`)
+		.eq('id', id)
+
+	if (response.error) console.log(response.error);
+	if (response.data.length === 0) console.log('This entry is not available in the database');
+
+	return response.data[0][`K_${a}`];
+}
