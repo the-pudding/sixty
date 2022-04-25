15682801c0546a4a9f423e6a1f81e9e4a894a2e5
russellgoldenberg <russellgoldenberg@gmail.com>
2022-04-25 07:23:09 -0400

    copy

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -113,7 +113,7 @@
     const url = `https://pudding.cool/2022/04/sixty-data/data.json?version=${Date.now()}`;
     const response = await fetch(url);
     $readerData = await response.json();
-    console.log($readerData);
+    // console.log($readerData);
   });
 </script>
 
