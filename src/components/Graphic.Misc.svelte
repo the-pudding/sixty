149cde7f9431e3b02815a8749199d14cfda78637
russellgoldenberg <russellgoldenberg@gmail.com>
2022-05-01 06:27:57 -0400

    misc

---
+++ src/components/Graphic.Misc.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { groups, format } from "d3";
+  import { readerData } from "$stores/misc.js";
+  $: exclude = $readerData.results.filter((d) => d.exclude);
+  $: console.table(exclude);
+  $: ratio = format(".2%")(exclude.length / $readerData.results.length);
+</script>
+
+<p>{ratio}</p>
